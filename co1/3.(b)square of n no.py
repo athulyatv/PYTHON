24cc7a3c6d161e2ee6efb3f8107ea62a0667261c
athulyatv <79806309+athulyatv@users.noreply.github.com>
2021-03-16 14:52:49 +0530

    Add files via upload

---
+++ co1/3.(b)square of n no.py
@@ -0,0 +1,9 @@
+#square of n numbers
+n=int(input("enter the limit:"))
+listi=[]
+for i in range(1,n+1):
+    listi.append(i)
+print(listi)
+newlist=[x**2 for x in listi]
+print(newlist)
+© 2021 GitHub, Inc.
