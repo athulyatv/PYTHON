fe8ce5883f5030ce3b7958fce1a79d3f674d6584
athulyatv <79806309+athulyatv@users.noreply.github.com>
2021-03-24 20:40:36 +0530

    cmt

---
+++ co5/4.print content of the column.py
@@ -0,0 +1,15 @@
+import csv
+
+header = ["place", "name", "age"]
+with open("city.csv", "w") as file:
+    write = csv.DictWriter(file, fieldnames=header)
+    write.writeheader()
+    write.writerow({"place": "kannur", "name": "Rinu", "age": 21})
+    write.writerow({"place": "kozhikod", "name": "Reeja", "age": 21})
+    write.writerow({"place": "kollam", "name": "Rojin", "age": 23})
+    write.writerow({"place": "Palakkaadu", "name": "Aleena", "age": 13})
+with open("city.csv", "r") as file:
+    read = csv.DictReader(file);
+    n = input("Enter the column name you want(place,name,age):")
+    for i in read:
+        print(i[n])
